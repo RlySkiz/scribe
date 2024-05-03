cca36d9b1e48884594366aecefa1432c870fa6d9
Skiz <35379280+RlySkiz@users.noreply.github.com>
2024-05-03 18:26:40 +0200

    Dump File

--- Mods/Scribe/ScriptExtender/Lua/Client/Scribe.lua
+++ Mods/Scribe/ScriptExtender/Lua/Client/Scribe.lua
@@ -1,6 +1,12 @@
 w = Ext.IMGUI.NewWindow("Scribe")
-w:AddText("Dump:")
-dumpArea = w:AddText("Dump Area")
+tabbar = w:AddTabBar()
+taba = tabbar:AddTabItem("Main")
+taba:AddText("Num_1 = dump | Num_2 = dump file")
+taba:AddText("Dump:")
+dumpArea = taba:AddText("Dump Area")
+
+tabb = tabbar:AddTabItem("Astarion")
+tabb:AddText("Dump")
 
 Ext.Events.KeyInput:Subscribe(function (e)
     if e.Event == "KeyDown" and e.Repeat == false then
@@ -8,7 +14,10 @@ Ext.Events.KeyInput:Subscribe(function (e)
         if e.Key == "NUM_1" then
             _P("Num_1 pressed")
             _D(Ext.IMGUI.GetPickingHelper(1))
-            dumpArea.Label = _D(Ext.IMGUI.GetPickingHelper(1))
+            --dumpArea.Label = _D(Ext.IMGUI.GetPickingHelper(1))
         end    
+        if e.Key == "NUM_2"
+        _P("Num_2 pressed")
+        Ext.IO.SaveFile("mouseoverDump.json", Ext.DumpExport(Ext.IMGUI.GetPickingHelper(1)))
     end
 end)
