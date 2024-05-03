3dbe603fb7f60ee4eb5986d95aeabf06cd0c9421
Skiz <35379280+RlySkiz@users.noreply.github.com>
2024-05-04 00:25:46 +0200

    Delete Mods/Scribe/ScriptExtender/Lua/Client/MouseoverTab.lua

--- Mods/Scribe/ScriptExtender/Lua/Client/MouseoverTab.lua
+++
@@ -1,8 +0,0 @@
-w = Ext.IMGUI.NewWindow("Scribe")
-tabbar = w:AddTabBar("")
-
-mouseoverTab = tabbar:AddTabItem("Main")
-mouseoverTab:AddText("Num_1 = dump | Num_2 = dump file")
-mouseoverTab:AddSeparator()
-mouseoverTab:AddText("Dump:")
-dumpMouseover = mouseoverTab:AddText("")
