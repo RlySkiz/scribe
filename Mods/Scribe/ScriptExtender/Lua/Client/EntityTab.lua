6e2c7fbb9816620dea67f322a56c185a0624df47
Skiz <35379280+RlySkiz@users.noreply.github.com>
2024-05-03 22:30:45 +0200

    SameLine fix

--- Mods/Scribe/ScriptExtender/Lua/Client/EntityTab.lua
+++ Mods/Scribe/ScriptExtender/Lua/Client/EntityTab.lua
@@ -2,19 +2,19 @@ entityTab = tabbar:AddTabItem("Entity")
 
 entityTab:AddText("Name: ")
 entityName = entityTab:AddText("")
-entityName.SameLine() = true
+entityName.SameLine = true
 
 entityTab:AddText("Race: ")
 entityRace = entityTab:AddText("")
-entityRace.SameLine() = true
+entityRace.SameLine = true
 
 entityTab:AddText("BodyType: ")
 entityBodyType = entityTab:AddText("")
-entityBodyType.SameLine() = true
+entityBodyType.SameLine = true
 
 entityTab:AddText("CharacterVisualResourceID: ")
 entityCVID = entityTab:AddText("")
-entityCVID.SameLine() = true
+entityCVID.SameLine = true
 
 entityTab:AddText("Dump")
 dumpEntity = entityTab:AddText("")
