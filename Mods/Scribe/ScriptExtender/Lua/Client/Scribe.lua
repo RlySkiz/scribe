665247e0ba894432d5834445e1be0830e524a334
Skiz <35379280+RlySkiz@users.noreply.github.com>
2024-05-03 20:11:40 +0200

    get Functions

--- Mods/Scribe/ScriptExtender/Lua/Client/Scribe.lua
+++ Mods/Scribe/ScriptExtender/Lua/Client/Scribe.lua
@@ -1,19 +1,31 @@
-mouseover = Ext.IMGUI.GetPickingHelper(1)
-entity = mouseover.Inner.Inner.Character
-item = mouseover.Inner.Inner.Item
+local function getMouseover()
+    return Ext.IMGUI.GetPickingHelper(1)
+end
+local function getEntity()
+    return Ext.IMGUI.GetPickingHelper(1).Inner.Inner.Character
+end
+local function getItem()
+    return Ext.IMGUI.GetPickingHelper(1).Inner.Inner.Item
+end
 
 Ext.Events.KeyInput:Subscribe(function (e)
     if e.Event == "KeyDown" and e.Repeat == false then
         _P("Something pressed")
+
         if e.Key == "NUM_1" then
             _P("Num_1 pressed")
             _D(Ext.IMGUI.GetPickingHelper(1))
-            dumpMouseover.Label = Ext.DumpExport(mouseover)
+            dumpMouseover.Label = Ext.DumpExport(getMouseover())
 
             if entity ~= nil then
-                dumpEntity.Label = Ext.DumpExport(Ext.Entity.Get(entity):GetAllComponents())
+                dumpEntity.Label = Ext.DumpExport(Ext.Entity.Get(getEntity()):GetAllComponents())
+            end
+
+            if getItem() ~= nil then
+                dumpEntity.Label = Ext.DumpExport(Ext.Entity.Get(getItem()):GetAllComponents())
             end
         end    
+
         if e.Key == "NUM_2" then
         _P("Num_2 pressed")
         Ext.IO.SaveFile("mouseoverDump.json", Ext.DumpExport(Ext.IMGUI.GetPickingHelper(1)))
